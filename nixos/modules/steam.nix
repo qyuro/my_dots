08d9d1369226063627a9ba9cad06a528977a8d59
qyuro <albertsvetsov@gmail.com>
2026-06-15 21:34:32 +0300

    update

--- nixos/modules/steam.nix
+++ nixos/modules/steam.nix
@@ -5,11 +5,11 @@
     #gamescopeSession.enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    # localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
     # package = pkgs.unstable.steam;
   };
-    # environment.sessionVariables = {
-    # STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/home/qyuro/.steam/root/compatibilitytools.d";
-  # };
+    environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/home/qyuro/.steam/root/compatibilitytools.d";
+  };
 
 }
